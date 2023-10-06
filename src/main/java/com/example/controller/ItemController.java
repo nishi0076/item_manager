417da0b5ce38ccdd3bcb96fbe8cfc0d2247cf90d
Nishida Tomoya <nishitomo7396@gmail.com>
2023-10-06 18:23:02 +0900

    lesson03実装完了

--- src/main/java/com/example/controller/ItemController.java
+++ src/main/java/com/example/controller/ItemController.java
@@ -31,26 +31,26 @@ public class ItemController {
 	// 商品一覧表示
 	@GetMapping
 	public String index(Model model) {
-		// データの疎通確認
 		List<Item> items = this.itemService.findAll();
-		// コンソールよりListの中身を確認する
-		System.out.println(items.toString());
+		// htmlのほうで利用する変数としてitemsをセット
+		model.addAttribute("items", items);
+		// 商品一覧を表示
 		return "item/index";
 	}
 	
 	
 	// 商品登録ページ表示
 	@GetMapping("toroku")
-	public String torokuPage(@ModelAttribute("itemform") ItemForm itemform) {
-		// 処理を追加
+	public String torokuPage(@ModelAttribute("itemForm") ItemForm itemForm) {
+		// 商品登録ページを表示
 		return "item/torokuPage";
 	}
 	
 	
 	// 商品登録実行
 	@PostMapping("toroku")
-	public String toroku(ItemForm itemform) {
-		// 処理を追加
+	public String toroku(ItemForm itemForm) {
+		this.itemService.save(itemForm);
 		return "redirect:/item";
 	}
 	
@@ -58,17 +58,22 @@ public class ItemController {
 	// 商品編集処理ページ表示
 	@GetMapping("hensyu/{id}")
 	public String hensyuPage(@PathVariable("id") Integer id, Model model
-			                  ,@ModelAttribute("itemform") ItemForm itemForm) {
-		// 処理を追加
+			                  ,@ModelAttribute("itemForm") ItemForm itemForm) {
+		// Entityクラスのインスタンスをidより検索し取得
+				Item item = this.itemService.findById(id);
+				// DTOクラスのフィールドにセット
+				itemForm.setName(item.getName());
+				itemForm.setPrice(item.getPrice());
+				// idをセット
+				model.addAttribute("id", id);
 		return "item/hensyuPage";
 	}
 	
 	
 	// 商品編集実行
 	@PostMapping("hensyu/{id}")
-	public String hensyu(@PathVariable("id") Integer id
-			              ,@ModelAttribute("itemform") ItemForm itemForm) {
-		// 処理を追加
+	public String hensyu(@PathVariable("id") Integer id, ItemForm itemForm) {
+		this.itemService.update(id, itemForm);
 		return "redirect:/item";
 	}
 	
@@ -76,7 +81,7 @@ public class ItemController {
 	// 商品削除実行
 	@PostMapping("sakujyo/{id}")
 	public String sakujyo(@PathVariable("id") Integer id) {
-		// 処理を追加
+		this.itemService.delete(id);
 		return "redirect:/item";
 	}
 }
--- src/main/java/com/example/service/ItemService.java
+++ src/main/java/com/example/service/ItemService.java
@@ -1,11 +1,13 @@
 package com.example.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.entity.Item;
+import com.example.form.ItemForm;
 import com.example.repository.ItemRepository;
 
 @Service
@@ -23,5 +25,43 @@ public class ItemService {
 	// データ全件取得
 	public List<Item> findAll() {
 		return this.itemRepository.findAll();
+	}
+	
+	
+	// データ保存用メソッド
+	public Item save(ItemForm itemForm) {
+		// entityクラスのインスタンス生成
+		Item item = new Item();
+		// DTOクラスからフィールドをセット
+		item.setName(itemForm.getName());
+		item.setPrice(itemForm.getPrice());
+		// saveメソッドでデータ保存
+		return this.itemRepository.save(item);
+	}
+	
+	
+	// IDカラムを利用してデータ検索
+	public Item findById(Integer id) {
+		Optional<Item> optionalItem = this.itemRepository.findById(id);
+		Item item = optionalItem.get();
+		return item;
+	}
+	
+	
+	// データ更新用メソッド
+	public Item update(Integer id, ItemForm itemForm) {
+		// データ１件分のEntityクラスを取得
+		Item item = this.findById(id);
+		// DTOクラスからフィールドをセット
+		item.setName(itemForm.getName());
+		item.setPrice(itemForm.getPrice());
+		// saveメソッドでデータ保存
+		return this.itemRepository.save(item);
+	}
+	
+	
+	// データ削除用メソッド
+	public void delete(Integer id) {
+		this.itemRepository.deleteById(id);
 	}
 }
