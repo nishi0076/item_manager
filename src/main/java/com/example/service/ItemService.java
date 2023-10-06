4c7ab38350216cd51ea870e5ae9de97258f9fb8e
Nishida Tomoya <nishitomo7396@gmail.com>
2023-10-06 14:21:14 +0900

    lesson02実装完了

---
+++ src/main/java/com/example/service/ItemService.java
@@ -0,0 +1,27 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Item;
+import com.example.repository.ItemRepository;
+
+@Service
+public class ItemService {
+    
+	private final ItemRepository itemRepository;
+	
+	// コンストラクタでrepositoryをインジェクション
+	@Autowired
+	public ItemService(ItemRepository itemRepository) {
+		this.itemRepository = itemRepository;
+	}
+	
+	
+	// データ全件取得
+	public List<Item> findAll() {
+		return this.itemRepository.findAll();
+	}
+}
